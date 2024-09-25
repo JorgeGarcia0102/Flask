27fbf56ce5fee2bce35fc13e42a7d6550e2ee91b
JorgeGarcia0102 <garciasilgadojorge@gmail.com>
2024-09-25 11:58:50 -0500

    Add files via upload

---
+++ 0. app_hellowork/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+
+    return "<h1>kike papi rico</h1>"
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, port=5000)
